1c262a30e7b65dd480391d8e6a313b1863986dae
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-24 21:58:06 +0200

    Update image position

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/future/image'
 const Home: NextPage = () => {
   return (
     <div className="flex-1 flex justify-center items-center">
-      <Image src="/portal.png" className="px-7 md:px-0" alt="Rick and Morty Logo" width={700} height={684} priority={true} />
+      <Image src="/portal.png" className="px-7 md:px-0 -mt-36 md:mt-0" alt="Rick and Morty Logo" width={700} height={684} priority={true} />
     </div>
   )
 }
