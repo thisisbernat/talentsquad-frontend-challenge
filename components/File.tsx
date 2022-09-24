86daa88b86d679b47de190d8d82eb3fcb9f44bf8
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-24 22:19:28 +0200

    New File component

---
+++ components/File.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/future/image'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
+import { HeartIcon as HeartIconSolid } from '@heroicons/react/24/solid'
+
+const File = () => {
+  return (
+    <div className="bg-zinc-700 my-10 mx-6 rounded-xl px-4 py-10 relative sm:max-w-3xl lg:mx-auto">
+      <div className="flex flex-col sm:flex-row sm:justify-evenly items-center sm:items-stretch">
+        <Image src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" className="min-w-60 min-h-60 rounded-full" alt="Rick Sanchez" height="300" width="300" priority={true} />
+        <div className="flex flex-col gap-1 mt-2 items-center md:justify-start sm:mt-4">
+          <div className="flex flex-col items-center">
+            <h1 className="text-white text-3xl font-black subpixel-antialiased">Rick Sanchez</h1>
+            <div className="flex items-center gap-1 font-medium">
+              <div className="h-3 w-3 bg-lime-500 rounded-full"></div>
+              <span>Alive</span>
+              -
+              <span>Human</span>
+              -
+              <span>Male</span>
+            </div>
+          </div>
+          <div className="md:flex md:flex-col md:gap-2 md:my-auto">
+            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+              <span className="text-gray-400 font-semibold">Origin:</span>
+              <span className="subpixel-antialiased">Earth (C-137)</span>
+            </div>
+            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+              <span className="text-gray-400 font-semibold">Last known location:</span>
+              <span className="subpixel-antialiased">Citadel of Ricks</span>
+            </div>
+            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+              <span className="text-gray-400 font-semibold">First seen in:</span>
+              <span className="subpixel-antialiased">Pilot</span>
+            </div>
+            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+              <span className="text-gray-400 font-semibold">Last seen in:</span>
+              <span className="subpixel-antialiased">Pilot</span>
+            </div>
+          </div>
+          <div className="absolute bottom-2 right-2">
+            <HeartIconOutline className="text-white h-10 w-10" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default File
