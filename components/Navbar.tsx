6635e10e2976737f64ce454c8e01367b98d23e15
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-24 21:06:42 +0200

    Fixed Navbar's responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,15 +13,15 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="bg-gradient-to-t from-zinc-800 to-zinc-900 px-5 pt-3 pb-10 text-white flex flex-col md:flex-row items-center justify-between">
+      <div className="bg-gradient-to-t from-zinc-800 to-zinc-900 px-5 pt-3 pb-10 text-white flex flex-col md:flex-row justify-between relative">
         <Link href="/">
           <div className="flex cursor-pointer">
             <Image src="/rickandmorty.svg" className="h-6 sm:h-9 w-auto" alt="Rick and Morty Logo" width={30} height={30} />
             <span className="ml-2 self-center whitespace-nowrap text-xl font-bold subpixel-antialiased">Rick and Morty</span>
           </div>
         </Link>
-        <div className="text-white h-6 w-6 md:hidden"><Bars3Icon /></div>
-        <div className="flex flex-col md:flex-row md:items-center gap-7 text-sm font-semibold pt-1">
+        <div className="text-white h-6 w-6 md:hidden absolute right-3 top-3"><Bars3Icon /></div>
+        <div className="flex flex-col md:flex-row items-center gap-7 text-sm font-semibold pt-5 md:pt-1">
           <Link href="/">
             <a className="hover:text-lime-600 hover:underline underline-offset-4">Home</a>
           </Link>
