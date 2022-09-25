d3be8bf8eb282501d81916be663b22a70ce96e77
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-25 09:19:10 +0200

    New random page

---
+++ pages/random.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/future/image'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
+import { HeartIcon as HeartIconSolid } from '@heroicons/react/24/solid'
+import File from '../components/File'
+
+const Random = () => {
+  return (
+    <File />
+  )
+}
+
+export default Random
