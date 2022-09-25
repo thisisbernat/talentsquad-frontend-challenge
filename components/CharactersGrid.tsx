5dfa3223e1db7a0b4673d3df0fd95443ded975e1
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-25 21:02:57 +0200

    Add Pagination to top too

--- components/CharactersGrid.tsx
+++ components/CharactersGrid.tsx
@@ -9,6 +9,7 @@ interface Props {
 const CharactersGrid = ({ children, className }: Props) => {
   return (
     <div>
+      <Pagination />
       <div className="lg:max-w-5xl 2xl:max-w-full mx-auto pt-10">
         <div className="grid gap-5 3xl:gap-0 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 px-2">
           {children}
