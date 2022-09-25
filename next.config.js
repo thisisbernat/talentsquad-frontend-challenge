685183ab9a5e3c3818ddf59308486fa1d8a5d12e
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-25 21:23:51 +0200

    Remove redirects

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['rickandmortyapi.com'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/characters',
-        destination: '/characters/page/1',
-        permanent: true,
-      },
-    ]
   }
 }
