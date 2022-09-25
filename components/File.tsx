1f234dace8c42f13d7b652d029b655e2d0812296
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-25 17:18:04 +0200

    Update literals from props

--- components/File.tsx
+++ components/File.tsx
@@ -1,39 +1,38 @@
+import type { NextPage } from 'next'
 import Image from 'next/future/image'
 import Heart from './Heart'
+import { Character } from '../types'
+import StatusIndicator from './StatusIndicator'
 
-const File = () => {
+const File: NextPage<{ character: Character, firstSeen: string, lastSeen: string }> = ({ character, firstSeen, lastSeen }) => {
   return (
-    <div className="bg-zinc-700 my-10 mx-6 rounded-xl px-4 py-10 relative sm:max-w-3xl lg:mx-auto">
+    <div className="bg-zinc-700 my-10 mx-12 md:mx-auto rounded-xl px-4 md:px-8 pt-8 pb-5 md:pb-8 relative">
       <div className="flex flex-col sm:flex-row sm:justify-evenly items-center sm:items-stretch">
-        <Image src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" className="min-w-60 min-h-60 rounded-full" alt="Rick Sanchez" height="300" width="300" priority={true} />
+        <Image src={character.image} className="min-w-60 min-h-60 rounded-full md:mr-4" alt={character.name} height="300" width="300" priority={true} />
         <div className="flex flex-col gap-1 mt-2 items-center md:justify-start sm:mt-4">
-          <div className="flex flex-col items-center">
-            <h1 className="text-white text-3xl font-black subpixel-antialiased">Rick Sanchez</h1>
-            <div className="flex items-center gap-1 font-medium">
-              <div className="h-3 w-3 bg-lime-500 rounded-full"></div>
-              <span>Alive</span>
-              -
-              <span>Human</span>
-              -
-              <span>Male</span>
+          <div className="flex flex-col items-center mt-2">
+            <h1 className="text-white text-3xl font-black subpixel-antialiased">{character.name}</h1>
+            <div className="flex items-center gap-1 font-medium text-white">
+              <StatusIndicator status={character.status} />
+              <span>{character.status}</span> - <span>{character.species}</span> - <span>{character.gender}</span>
             </div>
           </div>
-          <div className="md:flex md:flex-col md:gap-2 md:my-auto">
-            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+          <div className="md:flex md:flex-col md:items-center md:my-auto mt-2">
+            <div className="flex flex-col items-center">
               <span className="text-gray-400 font-semibold">Origin:</span>
-              <span className="subpixel-antialiased">Earth (C-137)</span>
+              <span className="subpixel-antialiased text-white">{character.origin.name}</span>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+            <div className="flex flex-col items-center">
               <span className="text-gray-400 font-semibold">Last known location:</span>
-              <span className="subpixel-antialiased">Citadel of Ricks</span>
+              <span className="subpixel-antialiased text-white">{character.location.name}</span>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+            <div className="flex flex-col items-center">
               <span className="text-gray-400 font-semibold">First seen in:</span>
-              <span className="subpixel-antialiased">Pilot</span>
+              <span className="subpixel-antialiased text-white">{firstSeen}</span>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-2 items-center">
+            <div className="flex flex-col items-center">
               <span className="text-gray-400 font-semibold">Last seen in:</span>
-              <span className="subpixel-antialiased">Pilot</span>
+              <span className="subpixel-antialiased text-white">{lastSeen}</span>
             </div>
           </div>
           <Heart size={'10'} active={true} />
