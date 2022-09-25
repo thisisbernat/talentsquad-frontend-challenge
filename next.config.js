6c6f529568aedd13f86950ac57462afc760d8f8c
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-25 21:02:43 +0200

    Add /characters redirection

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['rickandmortyapi.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/characters',
+        destination: '/characters/page/1',
+        permanent: true,
+      },
+    ]
   }
 }
