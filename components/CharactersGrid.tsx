aa2742922f6b303098e512083b29eb88c56f1ac8
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-29 11:46:16 +0200

    Remove unused prop

--- components/CharactersGrid.tsx
+++ components/CharactersGrid.tsx
@@ -2,10 +2,9 @@ import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
-  className?: string
 }
 
-const CharactersGrid = ({ children, className }: Props) => {
+const CharactersGrid = ({ children }: Props) => {
   return (
     <div className="lg:max-w-5xl 2xl:max-w-full mx-auto pt-10">
       <div className="grid gap-5 3xl:gap-0 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 px-2">
